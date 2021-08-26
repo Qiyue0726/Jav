33c0c7d3733ebcc39e1fac0190c8b3423423f606
qiyue <1301616594@qq.com>
2021-08-26 21:03:57 +0800

    小优化

--- Jav/settings.py
+++ Jav/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+f41yc-8oy-+xnr)7!39kp5y_046trnnl&w$^zthx^dxh$f-gk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
